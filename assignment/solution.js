21339a6f162caa675a91a4e4e084b97e736347da
faridah khaled <faridah.hashem@student.giu-uni.de>
2026-09-25 21:52:39 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,8 @@
+const name = "Faridah Khaled the Mage";
+const studentId = 16006199;
+const power = studentId * 9;
+
+console.log(name);
+console.log(studentId);
+console.log(power);
+console.log("By the Red Sea coast, the Chimera surrenders!");
